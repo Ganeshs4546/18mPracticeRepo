810f5e60645c6abdab759cce745ed841661e7c63
Ganesh <shinde300957@gmail.com>
2023-03-19 09:10:38 +0000

    commit from feature

--- file.java
+++ file.java
@@ -2,7 +2,7 @@ class umesh
 {
 public static void main(String[args])
 {
-system.out.println("change from local repo 18");
+system.out.println("change from local repo 18 2.0");
 syso("hi from feature-101");
 }
 }
